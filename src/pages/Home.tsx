8b6f572d80af4f32db667e2c8b7fb338215fe085
Kamat Enterprises <kamat.enterprises72@gmail.com>
2026-04-27 16:27:49 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -85,7 +85,7 @@ export default function Home() {
           <h2 className="text-sm font-bold text-gray-400 tracking-widest uppercase mb-8">Authorized Dealer Of</h2>
           <div className="flex flex-wrap justify-center items-center gap-8 md:gap-16 opacity-70 grayscale hover:grayscale-0 transition-all">
             <img src="public/images/logo1.JPG" alt="Berger" className="h-12 w-auto object-contain" referrerPolicy="no-referrer" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Asian_Paints_Logo.svg/1200px-Asian_Paints_Logo.svg.png" alt="Asian Paints" className="h-12 w-auto object-contain" referrerPolicy="no-referrer" />
+            <img src="public/images/logo2.png" alt="Asian Paints" className="h-12 w-auto object-contain" referrerPolicy="no-referrer" />
             <img src="https://www.dulux.in/content/dam/akzonobel-dulux/master/en/logos/logo-dulux-desktop.png" alt="Dulux" className="h-12 w-auto object-contain" referrerPolicy="no-referrer" />
             <img src="https://www.drfixit.co.in/sites/default/files/dr-fixit-logo.png" alt="Dr. Fixit" className="h-12 w-auto object-contain" referrerPolicy="no-referrer" />
           </div>
